4f4aaa5e3182a9e24c9f199499e998ea3df1ba8f
Philippe Huynh <2161459@cegepmontpetit.ca>
2025-09-23 16:19:01 -0400

    FCT Tiroir de navigation à finir
    Le tiroir est terminé pour la page d'accueil et la page de consultation. IL RESTE À L'AJOUTER À LA PAGE DE CRÉATION

--- tp1_flutter/lib/Consultaion.dart
+++ tp1_flutter/lib/Consultaion.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-
+import 'package:tp1_flutter/singleton.dart';
+import 'Accueil.dart';
+import 'Connexion.dart';
 import 'Creation.dart';
 
 class Consultaion extends StatelessWidget {
@@ -9,9 +11,51 @@ class Consultaion extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text("Consultation")),
+      drawer: Drawer(
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            UserAccountsDrawerHeader(
+              accountName: Text(UserSession().username),
+              accountEmail: null,
+              currentAccountPicture: const Icon(Icons.person),
+            ),
+            ListTile(
+              leading: const Icon(Icons.home),
+              title: const Text('Accueil'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => const HomeScreen()));
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.task),
+              title: const Text('Ajout de tâche'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => Creation(onAdd: (task, date) {}),
+                  ),
+                );
+              },
+            ),
+            ListTile(
+              leading: const Icon(Icons.logout),
+              title: const Text('Déconnexion'),
+              onTap: () {
+                Navigator.pop(context);
+                Navigator.pushReplacement(
+                  context,
+                  MaterialPageRoute(builder: (context) => Connexion()),
+                );
+              },
+            ),
+          ],
+        ),
+      ),
       body: const Center(child: Text("Bienvenue sur la page de consultation")),
-
     );
   }
 }
-
